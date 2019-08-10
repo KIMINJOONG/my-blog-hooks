a9eacf961548f4f821e8e37d960f20fdf2908d8c
KIMINJOONG <ijboym216@gmail.com>
2019-08-10 11:32:46 +0900

    ssr styled-components

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Header = styled.div`
+    width: 100%;
+    height: 124px;
+    border-bottom: 1px solid #707070;
+`;
+
+const Logo = styled.span`
+    width: 400px;
+    height: 124px;
+    font-family: SegoeUI;
+    font-size: 100px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.33;
+    letter-spacing: normal;
+    text-align: left;
+    color: #707070;s
+`;
 
 const AppLayout = ({ children }) => {
     return (
         <div>
-            <div>
+            <Header>
                 <Link href="/">
-                    <a>myblog</a>
+                    <Logo>DRINKER</Logo>
                 </Link>
-            </div>
+            </Header>
             <div>
                 {children}
             </div>
