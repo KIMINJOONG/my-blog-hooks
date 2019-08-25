e2a39fecd5f94cbae7bffb79dc9d4b07a8e56491
KIMINJOONG <ijboym216@gmail.com>
2019-08-25 15:16:26 +0900

    change address

--- sagas/index.js
+++ sagas/index.js
@@ -3,7 +3,7 @@ import user from './user';
 import board from './board';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'http://localhost:4000/';
 
 export default function* rootSaga() {
     yield all([call(user), call(board)]);
