74c3830fbe33bb2d128ea38ec8494bc3ec2645a8
KIMINJOONG <ijboym216@gmail.com>
2019-09-02 00:08:34 +0900

    change url

--- sagas/index.js
+++ sagas/index.js
@@ -7,8 +7,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // axios.defaults.baseURL = 'http://ec2-54-180-128-126.ap-northeast-2.compute.amazonaws.com:4000';
-// axios.defaults.baseURL = 'http://kohubi.me:4000';
-axios.defaults.baseURL = 'http://52.79.32.200:4000';
+axios.defaults.baseURL = 'http://kohubi.me:4000';
+// axios.defaults.baseURL = 'http://52.79.32.200:4000';
 // axios.defaults.baseURL = process.env.BASE_URL;
 // axios.defaults.baseURL = 'http://localhost:4000';
 
