2369ebedb1dde14605eb2f61f345ab529851a530
KIMINJOONG <ijboym216@gmail.com>
2019-09-02 00:07:13 +0900

    change url

--- sagas/index.js
+++ sagas/index.js
@@ -2,9 +2,14 @@ import { all, call } from 'redux-saga/effects';
 import user from './user';
 import board from './board';
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // axios.defaults.baseURL = 'http://ec2-54-180-128-126.ap-northeast-2.compute.amazonaws.com:4000';
-axios.defaults.baseURL = 'http://kohubi.me:4000';
+// axios.defaults.baseURL = 'http://kohubi.me:4000';
+axios.defaults.baseURL = 'http://52.79.32.200:4000';
+// axios.defaults.baseURL = process.env.BASE_URL;
 // axios.defaults.baseURL = 'http://localhost:4000';
 
 export default function* rootSaga() {
