b4b530b62115283e6c7c378c9fe02aabfdb86b4d
KIMINJOONG <ijboym216@gmail.com>
2019-10-05 10:41:44 +0900

    change api url

--- sagas/index.js
+++ sagas/index.js
@@ -4,7 +4,7 @@ import board from './board';
 import boards from './boards';
 import axios from 'axios';
 
-const baseURL = (process.env.NODE_ENV === "production" ? 'https://api.kohubi.me' : 'http://localhost:4000');
+const baseURL = (process.env.NODE_ENV === "production" ? 'https://api.kohubi.me/api' : 'http://localhost:4000/api');
 // axios.defaults.baseURL = 'http://kohubi.me:4000';
 // axios.defaults.baseURL = process.env.DEV_SERVER;
 // axios.defaults.baseURL = 'http://localhost:4000';
